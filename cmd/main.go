3acd85f4bc339922d3beb13ffa047cd24050b211
RUSLAN_MOZH_AKAD <RUSLAN_MOZH2003>
2025-11-03 05:33:07 -0500

    Изменеия в коде_1

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"todo"
+	handler "todo/pkg/handlers"
+)
+
 func main() {
+	handler := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handler.InitRoutes()); err != nil {
+		log.Fatalf("ОШИБКА ЗАПУСКА ПРИЛОЖЕНИЯ %s", err.Error())
+	}
 
 }
---
+++ server.go
@@ -0,0 +1,26 @@
+package todo
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, hanler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		Handler:        hanler,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+	}
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
